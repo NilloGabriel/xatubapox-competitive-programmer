c374b77e709dbd11f561e195bf4bbadf10b58519
Gabriel D' Nillo <gabrieldnillo99@gmail.com>
2022-09-17 09:40:21 -0300

    add solution for problem 2454

---
+++ weekly/week-1/2454-fliper.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int p, r;
+
+  cin >> p >> r;
+
+  if (p == 1 && r == 0)
+    cout << "B" << endl;
+  else if (p == 1 && r == 1)
+    cout << "A" << endl;
+  else
+    cout << "C" << endl;
+  
+  return 0;
+}
